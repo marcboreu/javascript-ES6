037a03b7baaab300099346488791880f3cfeb93b
Marc Boreu <marcboreu@gmail.com>
2020-09-16 18:45:21 +0200

    Exercici-3

---
+++ Exercici3.js
@@ -0,0 +1,25 @@
+// Exercici 3
+// Arregla l'error del següent bloc de codi:
+
+// users.map(function (user) {
+//     user.firstName;
+//  });
+
+//  var users = [{ firstName: 'Homer', lastName: 'Simpson' },
+//  { firstName: 'Marge', lastName: 'Simpson' },
+//  { firstName: 'Bart', lastName: 'Simpson' },
+//  { firstName: 'Lisa', lastName: 'Simpson' },
+//  { firstName: 'Maggie', lastName: 'Simpson' }];
+
+var users = [
+    { firstName: 'Homer', lastName: 'Simpson' },
+    { firstName: 'Marge', lastName: 'Simpson' },
+    { firstName: 'Bart', lastName: 'Simpson' },
+    { firstName: 'Lisa', lastName: 'Simpson' },
+    { firstName: 'Maggie', lastName: 'Simpson' }
+];
+
+users.map(user => user.firstName);
+
+
+
